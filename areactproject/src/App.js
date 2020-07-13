59ea418aba5b14f2ff6000654bbfc191e023bedf
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-07-13 18:28:12 +0200

    Deletin"testReact.js"AfterDoneWithSuccefulTestEtc

--- areactproject/src/App.js
+++ areactproject/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import TestFunc from"./testReact";
 
 function App() {
   
   return (
     <div>
       <h1>Testing Component-App.js Component</h1>
-      <TestFunc />
+      
     </div>
   );
 }
