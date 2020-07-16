e1a2ebf25aa40c0298fefa3a3a424c7c962900b3
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-07-16 19:13:16 +0200

    AddClassTweetToDivUsingClassName(KuzItsJavascript)

--- areactproject/src/Tweet.js
+++ areactproject/src/Tweet.js
@@ -3,7 +3,7 @@ import React from "react"
 
 function Tweet(){
     return(
-        <div>
+        <div className="tweet">
             <h3>Name Of User Or Tweeter</h3>
             <p>Tweet Message for test</p>
             <h3>Likes Number</h3>
