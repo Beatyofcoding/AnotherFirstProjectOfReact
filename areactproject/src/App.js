633f16cf84eae395b4c43fc7059d54877b6f36a6
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-07-23 13:13:43 +0200

    AddingTweetMessageFor"tweet2"

--- areactproject/src/App.js
+++ areactproject/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       {/* <h1>TestingApp.js</h1> */}
       <Tweet name="tweet1" message="Hello This Is A Test For Tweet1"/>
-      <Tweet name="tweet2"/>
+      <Tweet name="tweet2" message="Hi This Is Another Test For Tweet2"/>
       <Tweet name="tweet3"/>
       <Tweet name="tweet4"/>
 
