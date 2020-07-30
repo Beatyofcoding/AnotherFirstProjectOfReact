483780c25403d08d3a3379c3928637364a9ad587
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-07-30 02:49:15 +0200

    SettingMsgsOfTweetsOnJSX&RenderingItSuccessfuly

--- areactproject/src/App.js
+++ areactproject/src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       {users.map(user =>(
-        <Tweet name={user.name} />
+        <Tweet name={user.name} message={user.message} />
       ))}
       <h1 className={isGreen ? "green" : ""}>Toggle Switch Color On-Off</h1>
       <button onClick={incrementing}>Increment Button</button>
